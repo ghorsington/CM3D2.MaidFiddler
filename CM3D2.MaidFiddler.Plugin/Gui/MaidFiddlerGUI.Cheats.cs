d82d507bcd8a3a90d0132effe920f7aeb0bfe759
Denikson <mrdenikson@gmail.com>
2015-11-22 21:26:51 +0200

    Fixed missing references
    Fixed missing references to LINQ extension methods

--- CM3D2.MaidFiddler.Plugin/Gui/MaidFiddlerGUI.Cheats.cs
+++ CM3D2.MaidFiddler.Plugin/Gui/MaidFiddlerGUI.Cheats.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using CM3D2.MaidFiddler.Plugin.Utils;
 using param;
--- CM3D2.MaidFiddler.Plugin/Gui/MaidFiddlerGUI.Skills.cs
+++ CM3D2.MaidFiddler.Plugin/Gui/MaidFiddlerGUI.Skills.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using CM3D2.MaidFiddler.Plugin.Utils;
 
